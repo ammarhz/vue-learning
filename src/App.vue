bc38843423050949e85245d4b5a35768ee63afae
ammarhz <ammarhz.7j@gmail.com>
2018-11-12 07:13:50 -0600

    fixed background color issue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
   body {
     background-color: #c4c4c4;
   }
--- src/components/SignUpButton.vue
+++ src/components/SignUpButton.vue
@@ -16,7 +16,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .container {
     width: 10%;
   }
